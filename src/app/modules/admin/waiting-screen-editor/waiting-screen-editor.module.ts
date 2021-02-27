a0ac292420072d82bc9321435ec7b967819ae6f0
kirjs <kirill@kcherkashin.org>
2021-02-27 14:21:37 -0500

    Add angular material

--- src/app/modules/admin/waiting-screen-editor/waiting-screen-editor.module.ts
+++ src/app/modules/admin/waiting-screen-editor/waiting-screen-editor.module.ts
@@ -5,6 +5,9 @@ import { WaitingScreenModule } from '../../waiting-screen/waiting-screen.module'
 import { FormsModule } from '@angular/forms';
 import { AnnounceModule } from '../../announce/announce.module';
 import { LoginModule } from '../login/login.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [WaitingScreenEditorComponent],
@@ -14,6 +17,9 @@ import { LoginModule } from '../login/login.module';
     FormsModule,
     AnnounceModule,
     LoginModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
   ],
 })
 export class WaitingScreenEditorModule {}
