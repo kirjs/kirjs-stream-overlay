31b35811878532e97b408cbf96aeb03f449f6b55
kirjs <kirill@kcherkashin.org>
2021-05-22 15:26:37 -0400

    Make header mobile first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,37 @@
-import {Injectable, NgModule} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterModule, RouterStateSnapshot, Routes} from '@angular/router';
+import {Injectable, NgModule, OnDestroy} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterModule, RouterStateSnapshot, Routes, UrlTree} from '@angular/router';
 import {OverlayComponent} from './modules/overlay/overlay.component';
 import {AnnounceComponent} from './modules/announce/announce.component';
 import {StreamCompleteComponent} from './modules/stream-complete/stream-complete.component';
 import {WaitingScreenWrapperComponent} from './modules/waiting-screen/waiting-screen-wrapper.component';
 import {HomeComponent} from './modules/home/home.component';
 import {StreamConfigService} from './modules/admin/stream-manager/stream-config.service';
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 
 // Using this one weird trick to redirect to an external URL.
 @Injectable()
-class TalkRedirectGuard implements CanActivate {
-  constructor(contentService: StreamConfigService) {
-    contentService.currentStream$.subscribe((a) => {
-      if(a && a.talkUrl){
-        window.location.href = a.talkUrl;
-      }
+class TalkRedirectGuard implements CanActivate, OnDestroy {
+  readonly destroy$ = new Subject();
 
-    });
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  constructor(contentService: StreamConfigService) {
+    contentService.currentStream$
+      .pipe(takeUntil(this.destroy$)).subscribe(
+      (a) => {
+        if (a && a.talkUrl) {
+          window.location.href = a.talkUrl;
+        }
+      });
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+    return new UrlTree();
   }
 }
 
