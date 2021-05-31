bf4d5977b499791076f0d166861f1e1cf65fb147
kirjs <kirill@kcherkashin.org>
2021-05-31 13:52:42 -0400

    Hide chat in the overlay if the appropriate setting is disabled

--- src/app/modules/overlay/chat/chat.component.ts
+++ src/app/modules/overlay/chat/chat.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { TwitchService } from '../../admin/services/twitch';
-import { StreamConfigService } from '../../admin/stream-manager/stream-config.service';
-import {ChatService} from "./chat.service";
+import {Component, OnInit} from '@angular/core';
+import {StreamConfigService} from '../../admin/stream-manager/stream-config.service';
+import {ChatService} from './chat.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
@@ -9,10 +9,17 @@ import {ChatService} from "./chat.service";
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+  readonly shouldShowChat$ = this.streamConfigService.currentStream$.pipe(map(stream => {
+    // tslint:disable-next-line:no-non-null-assertion
+    return stream!.showChat;
+  }));
+
   constructor(
     readonly streamConfigService: StreamConfigService,
     readonly chatService: ChatService,
-  ) {}
+  ) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
