df63aac0a42beb0d6ef1fab63b8ed3124c32a91b
kirjs <kirill@kcherkashin.org>
2021-07-14 22:30:13 +0300

    Fetch twitch user avatars

--- src/app/modules/overlay/chat/chat.service.ts
+++ src/app/modules/overlay/chat/chat.service.ts
@@ -1,46 +1,51 @@
-import {Injectable} from '@angular/core';
-import {EMPTY, merge, Observable} from 'rxjs';
-import {TwitchService} from '../../admin/services/twitch';
-import {catchError, filter, map, scan, switchMap} from 'rxjs/operators';
-import {YoutubeService} from '../../admin/services/youtube.service';
-import {StreamConfigService} from '../../admin/stream-manager/stream-config.service';
-import {ChatMessage} from './types';
-import {maxChatTimeout} from './common';
-
+import { Injectable } from '@angular/core';
+import { EMPTY, merge, Observable } from 'rxjs';
+import { catchError, filter, map, scan, switchMap } from 'rxjs/operators';
+import { TwitchService } from '../../admin/services/twitch';
+import { YoutubeService } from '../../admin/services/youtube.service';
+import { StreamConfigService } from '../../admin/stream-manager/stream-config.service';
+import { maxChatTimeout } from './common';
+import { ChatMessage } from './types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
-  readonly youtubeChat$ = this.streamConfigService.currentStream$
-    .pipe(
-      filter(stream => !!stream && !!stream.lapteuhYoutubeLiveChatId),
-      switchMap(currentStream => {
-        return this.youtube.fetchChat(currentStream!.lapteuhYoutubeLiveChatId);
-      })
-    );
+  readonly youtubeChat$ = this.streamConfigService.currentStream$.pipe(
+    filter(stream => !!stream && !!stream.lapteuhYoutubeLiveChatId),
+    switchMap(currentStream => {
+      return this.youtube.fetchChat(currentStream!.lapteuhYoutubeLiveChatId);
+    }),
+  );
 
   readonly latestMessages$: Observable<ChatMessage[]> = merge(
     this.twitch.messages$,
-    this.youtubeChat$.pipe(catchError(() => EMPTY))
-  )
-    .pipe(scan((messages: ChatMessage[], message) => {
+    this.youtubeChat$.pipe(
+      catchError(error => {
+        console.error(error);
+        return EMPTY;
+      }),
+    ),
+  ).pipe(
+    scan((messages: ChatMessage[], message) => {
       const now = +new Date();
-      messages = messages.filter(m => now - Number(m.timestamp) < maxChatTimeout);
+      messages = messages.filter(
+        m => now - Number(m.timestamp) < maxChatTimeout,
+      );
       messages.push(...message);
       return messages;
-    }, [] as ChatMessage[]));
+    }, [] as ChatMessage[]),
+  );
 
-  chat$: Observable<ChatMessage[]> = this.latestMessages$
-    .pipe(map((messages) => {
-      return messages
-        .sort((a, b) => Number(a.timestamp) - Number(b.timestamp));
-    }));
+  chat$: Observable<ChatMessage[]> = this.latestMessages$.pipe(
+    map(messages => {
+      return messages.sort((a, b) => Number(a.timestamp) - Number(b.timestamp));
+    }),
+  );
 
   constructor(
     private readonly streamConfigService: StreamConfigService,
     private readonly twitch: TwitchService,
     private readonly youtube: YoutubeService,
-  ) {
-  }
+  ) {}
 }
