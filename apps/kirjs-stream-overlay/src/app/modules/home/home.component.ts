4bea543f3bfb29ccbe44c8fbe1fd1f4bf455286d
kirjs <kirill@kcherkashin.org>
2023-11-15 12:41:33 -0500

    Upgrade home component to the new control flow

--- apps/kirjs-stream-overlay/src/app/modules/home/home.component.ts
+++ apps/kirjs-stream-overlay/src/app/modules/home/home.component.ts
@@ -1,28 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { StreamConfigService } from '../admin/stream-manager/stream-config.service';
-import { UIStream } from '../admin/stream-manager/types';
 import { AnnounceComponent } from '../announce/announce.component';
-import { NgIf, NgFor, AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
 import { AdminButtonComponent } from './admin-button/admin-button.component';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports: [
-        AdminButtonComponent,
-        NgIf,
-        NgFor,
-        AnnounceComponent,
-        AsyncPipe,
-        DatePipe,
-    ],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [AdminButtonComponent, AnnounceComponent, AsyncPipe, DatePipe],
 })
 export class HomeComponent {
-  constructor(readonly streamConfigService: StreamConfigService) {}
-
-  trackByKey(i: number, object: UIStream): string {
-    return object.key;
-  }
+  protected readonly streamConfigService = inject(StreamConfigService);
 }
